8aeaa0e2a0635b4bf8c5262ae2af0e613ead7f82
Julien Amblard <amblard.julien@gmail.com>
2020-03-11 10:26:19 +0100

    update TS

--- src/lettersComparison.ts
+++ src/lettersComparison.ts
@@ -10,8 +10,9 @@ interface LettersComparisonConfigInterface {
 
 export const lettersComparison:LettersComparisonConfigInterface = ( value = "", misspelledLength = 1 ) => ( ref ) => {
 
-	const valueLetters= value.split("")
-	const refLetters= ref.split("")
+	const valueLetters:string[]  = value.split("")
+	const refLetters:string[] = ref.split("")
+
 	const remainsvalueLetters = valueLetters.filter( letter => {
 		const refLetterIndex: number = refLetters.indexOf(letter)
 		const haseLetter: boolean = refLetterIndex > -1
