a9df2e866016246a2e01db6e6bbaaeb91c3bb745
Julien Amblard <amblard.julien@gmail.com>
2020-03-11 11:16:16 +0100

    change string comparison algo

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,4 +2,5 @@ import { emailChecker } from "../../src"
 const checker = emailChecker()
 
 console.log( checker("zefzefzef@outloook.com") )
+console.log( checker("zefzefzef@kooltuo.com") )
 console.log( checker("zefzefzef@gmial.com") )
--- src/lettersComparison.ts
+++ src/lettersComparison.ts
@@ -7,18 +7,22 @@ interface LettersComparisonConfigInterface {
 		misspelledLength: number
 	): LettersComparisonInterface
 }
-
+//https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/string/levenshtein-distance
 export const lettersComparison:LettersComparisonConfigInterface = ( value = "", misspelledLength = 1 ) => ( ref ) => {
+	const distanceMatrix = Array(value.length + 1).fill(null).map(() => Array(ref.length + 1).fill(null))
 
-	const valueLetters:string[]  = value.split("")
-	const refLetters:string[] = ref.split("")
-
-	const remainsvalueLetters = valueLetters.filter( (letter: string):boolean => {
-		const refLetterIndex: number = refLetters.indexOf(letter)
-		const haseLetter: boolean = refLetterIndex > -1
-		if( haseLetter ) refLetters.splice(refLetterIndex, 1)
-		return !haseLetter
-	})
-
-	return (remainsvalueLetters.length + refLetters.length) <= misspelledLength
+	for( let i = 0; i <= ref.length; i+=1 ) distanceMatrix[0][i] = i
+	for( let j = 0; j <= value.length; j+=1 ) distanceMatrix[j][0] = j
+	
+	for( let j = 1; j <= value.length; j+=1 ){
+		for( let i = 1; i <= ref.length; i+=1 ) {
+			const indicator = ref[i - 1] === value[j - 1] ? 0 : 1
+			distanceMatrix[j][i] = Math.min(
+				distanceMatrix[j][i - 1] + 1,
+				distanceMatrix[j - 1][i] + 1,
+				distanceMatrix[j - 1][i - 1] + indicator
+			)
+		}
+	}
+	return distanceMatrix[value.length][ref.length] <= misspelledLength
 }
