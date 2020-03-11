60aa571a7021cf80938d067c655013008bf0f3f5
Julien Amblard <amblard.julien@gmail.com>
2020-03-11 21:25:01 +0100

    udpate tu

--- tests/src/StringLengthChecker.test.js
+++ tests/src/StringLengthChecker.test.js
@@ -1,25 +1,24 @@
 import { stringLengthChecker } from "../../src/stringLengthChecker"
 
 describe("stringLengthChecker with diff of 1 : ", () => {
-	const max = 1
 	test("abcd compare efgh to should return true", () => {
-		const checker = stringLengthChecker("abcd", max)
+		const checker = stringLengthChecker("abcd")
 		expect(checker("efgh")).toBe(true)
 	})
 	test("abcd compare efg to should return true", () => {
-		const checker = stringLengthChecker("abcd", max)
+		const checker = stringLengthChecker("abcd")
 		expect(checker("efg")).toBe(true)
 	})
 	test("abcd compare ef to should return true", () => {
-		const checker = stringLengthChecker("abcd", max)
+		const checker = stringLengthChecker("abcd")
 		expect(checker("ef")).toBe(false)
 	})
 	test("abcd compare e to should return true", () => {
-		const checker = stringLengthChecker("abcd", max)
+		const checker = stringLengthChecker("abcd")
 		expect(checker("e")).toBe(false)
 	})
 	test("abcd compare efghj to should return true", () => {
-		const checker = stringLengthChecker("abcd", max)
+		const checker = stringLengthChecker("abcd", 1)
 		expect(checker("efghj")).toBe(true)
 	})
 	test("abcd compare efghjk to should return false", () => {
@@ -28,6 +27,10 @@ describe("stringLengthChecker with diff of 1 : ", () => {
 	})
 	test("abcd compare efghjkl to should return false", () => {
 		const checker = stringLengthChecker("abcd", max)
+		expect(checker("efghjkl")).toBe(false)
+	})
+	test("nothing compare efghjkl to should return false", () => {
+		const checker = stringLengthChecker()
 		expect(checker("efghjkl")).toBe(false)
 	})
 })
