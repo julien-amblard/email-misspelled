8df60e7f796c083bae67878a67ec27253c5c3a67
julien amblard <julien.amblard@hermes.com>
2024-03-02 17:56:01 +0100

    fix: ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     globals: true,
-    environment: "node"
+    environment: "node",
+    coverage: {
+      provider: "v8"
+    }
   }
 })
